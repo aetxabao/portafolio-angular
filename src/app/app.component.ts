0b764d9084c8b073b1dfd4bb7bc2d717095f1997
aetxabao <aitor.etxabarren@mariaanasanz.es>
2024-11-01 11:32:17 +0100

    Páginas adicionales, rutas y constantes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from "./shared/footer/footer.component";
 import { PortafolioComponent } from "./pages/portafolio/portafolio.component";
+import { AboutComponent } from "./pages/about/about.component";
+import { ItemComponent } from "./pages/item/item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, PortafolioComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, PortafolioComponent, AboutComponent, ItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/portafolio/portafolio.component.ts
+++ src/app/pages/portafolio/portafolio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-portafolio',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './portafolio.component.html',
   styleUrl: './portafolio.component.css'
 })
